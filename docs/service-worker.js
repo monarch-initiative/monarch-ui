ecc01f774ea588136a7cef05e739adca82600c37
DoctorBud <dan@quantumclay.com>
2018-09-26 16:46:05 -0700

    Just a publish-to-docs build to show off PR#6

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 importScripts(
-  "/monarch-ui/precache-manifest.f98a6206eae8cb65060539abdb800747.js"
+  "/monarch-ui/precache-manifest.c514ac7ad0f52067bde4a2d828945212.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "monarch-ui"});
