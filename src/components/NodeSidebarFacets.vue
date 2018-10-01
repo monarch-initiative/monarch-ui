a5ad07ac9fae41c79469426836a1533e81068a28
Nathan Dunn <ndunn@me.com>
2018-10-01 14:02:40 -0700

    - Rename AssocFacets.vue to be NodeSidebarFacets.vue. - Fix sidebar facets to be a flyover panel. - Adjust neighborhood/facet flyovers so that their trigger button is   more clearly associated with the panel. - Fix it so that both flyovers cannot display at the same time

---
+++ src/components/NodeSidebarFacets.vue
@@ -0,0 +1,79 @@
+<template>
+  <div
+    id="facets"
+    :class="{ active: isVisible }">
+
+    <div
+      v-for="(value, key) in model"
+      :key="key"
+      class="facet-item">
+
+      <b-form-checkbox
+        v-model="model[key]"
+      >
+        <i>{{ key }}</i>
+      </b-form-checkbox>
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'NodeSidebarFacets',
+
+  model: {
+    prop: 'model',
+  },
+
+  props: {
+    model: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+    isVisible: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+
+</script>
+
+<style lang="scss">
+@import "~@/style/variables";
+
+$facets-width: 300px;
+
+#facets {
+  width: $facets-width;
+  position: fixed;
+  top: ($navbar-height + 105);
+  left: (-$facets-width);
+  min-height: 40px;
+  z-index: 1050;
+  transition: all 0.3s;
+  overflow-y: auto;
+  overflow-x: hidden;
+  background: ghostwhite;
+  border:2px solid gray;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+#facets.active {
+  left: $collapsed-sidebar-width;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+}
+
+
+#facets .facet-item {
+  background: white;
+  padding: 0;
+}
+
+</style>
---
+++ src/components/NodeSidebarNeighborhood.vue
@@ -0,0 +1,138 @@
+<template>
+
+  <nav
+    id="neighborhood"
+    :class="{ active: isVisible }">
+
+    <div class="neighborhood">
+      <div
+        v-for="c in superclasses"
+        :key="c.id"
+        class="row superclass">
+        <div class="col-12">
+          <router-link
+            :to="'/' + nodeType + '/' + c.id">
+            {{ c.label }}
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row currentclass">
+        <div class="col-12">
+          {{ nodeLabel }}
+        </div>
+      </div>
+
+      <div
+        v-for="c in subclasses"
+        :key="c.id"
+        class="row subclass">
+        <div class="col-12">
+          <router-link
+            :to="'/' + nodeType + '/' + c.id">
+            {{ c.label }}
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'NodeSidebarNeighborhood',
+
+  props: {
+    nodeType: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    nodeLabel: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    superclasses: {
+      type: Array,
+      required: false,
+      default: null,
+    },
+    subclasses: {
+      type: Array,
+      required: false,
+      default: null,
+    },
+    isVisible: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+
+</script>
+
+
+<style lang="scss">
+
+@import "~@/style/variables";
+
+$neighborhood-width: 500px;
+$neighborhood-button-width: 32px;
+
+#neighborhood {
+  width: $neighborhood-width;
+  position: fixed;
+  top: ($navbar-height + 40);
+  left: (-$neighborhood-width);
+  min-height: 40px;
+  z-index: 1050;
+  transition: all 0.3s;
+  overflow-y: auto;
+  overflow-x: hidden;
+  background: ghostwhite;
+  border:2px solid lightgray;
+  border-radius: 5px;
+}
+
+#neighborhood.active {
+  left: $collapsed-sidebar-width;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+}
+
+#neighborhood a,
+#neighborhood a:hover,
+#neighborhood a:focus {
+  padding: 2px 10px;
+  color: inherit;
+  text-decoration: none;
+  transition: all 0.3s;
+  border: 1px solid transparent;
+}
+
+#neighborhood .neighborhood .superclass {
+  margin-left: 0;
+}
+
+#neighborhood .neighborhood .currentclass {
+  font-weight: 600;
+  margin-left: 15px;
+}
+
+#neighborhood .neighborhood .subclass {
+  margin-left: 30px;
+}
+
+#neighborhood .subclass a:hover,
+#neighborhood .superclass a:hover,
+#neighborhood .currentclass a:hover {
+  color: black;
+  background: #fff;
+  border-color: lightblue;
+}
+
+</style>
