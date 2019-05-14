7ad929abd51d6ca518d7d8b87a98c7f7af581ac4
cmungall <cjm@berkeleybop.org>
2019-05-13 17:21:58 -0700

    lint fixed

--- src/api/Sources.js
+++ src/api/Sources.js
@@ -4,20 +4,25 @@ import yaml from 'js-yaml';
 // this will be replaced when Deepak adds biolink route
 export default async function getSources() {
 
-    return {"sources": 
+  return {
+    'sources':
      [
 
-         {"id": "OMIM",
-          "url": "http://omim.org",
-          "title" : "Online Mendelian Inheritance in Man",
-          "sourceDate": "2025-01-01"},
-         
-         {"id": "PomBase", 
-          "url": "http://pombase.org",
-          "title" : "Pombe Model Organism Database",
-          "sourceDate": "2025-01-01"}
-         
+       {
+         'id': 'OMIM',
+         'url': 'http://omim.org',
+         'title': 'Online Mendelian Inheritance in Man',
+         'sourceDate': '2025-01-01'
+       },
+
+       {
+         'id': 'PomBase',
+         'url': 'http://pombase.org',
+         'title': 'Pombe Model Organism Database',
+         'sourceDate': '2025-01-01'
+       }
+
      ]
-    }
+  };
 
 }
--- src/views/Sources.vue
+++ src/views/Sources.vue
@@ -13,13 +13,14 @@
       </div>
       <div
         v-for="(source, index) in sources"
+        :key="index"
         class="row">
         <div class="col-1"> {{ source.id }}</div>
         <div class="col-6"> {{ source.title }}</div>
         <div class="col-2"> {{ source.sourceDate }}</div>
         <div class="col-3"> <a
-                              target="_blank"
-                              :href="source.url">{{ source.url }}</a></div>
+          :href="source.url"
+          target="_blank">{{ source.url }}</a></div>
       </div>
     </div>
     <monarch-footer/>
@@ -43,8 +44,8 @@ export default {
     this.sources = (await getSources()).sources;
     console.log('Num sources=', this.sources);
   },
-  //mounted() {
-  //}
+  // mounted() {
+  // }
 };
 </script>
 
