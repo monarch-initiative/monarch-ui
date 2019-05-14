d7c4cdf64d69d8983b37156cb64afb96ed398b8e
Nathan Dunn <ndunn@me.com>
2019-05-14 10:58:13 -0700

    fixed taxon lookup loop

--- tests/unit/taxon.spec.js
+++ tests/unit/taxon.spec.js
@@ -12,9 +12,11 @@ import { labelToId, idToLabel} from '../../src/lib/TaxonMap';
 
 describe('taxon lookups', () => {
   it('test taxon lookups', () => {
-    expect('NCBITaxon:78454', labelToId('Saguinus fuscicollis'));
-    expect('NCBITaxon:10116', labelToId('Rattus norvegicus'));
-    expect(idToLabel('NCBITaxon:78454'), 'Saguinus fuscicollis');
-    expect(idToLabel('NCBITaxon:10116'), 'Rattus norvegicus');
+    expect(idToLabel('NCBITaxon:78454')).to.equal('Saguinus labiatus');
+    expect(idToLabel('NCBITaxon:10116')).to.equal('Rattus norvegicus');
+    expect(idToLabel('NCBITaxon:10BAD')).to.equal(undefined);
+    expect('NCBITaxon:78454').to.equal(labelToId('Saguinus labiatus'));
+    expect('NCBITaxon:10116').to.equal(labelToId('Rattus norvegicus'));
+    expect(null).to.equal(labelToId('Rattus bad'));
   });
 });
