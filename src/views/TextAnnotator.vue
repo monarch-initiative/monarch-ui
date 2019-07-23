9a3a3a96f9ad12e7c1b630ed4c3c49f2f22a39c5
Michael Gargano <Michael.Gargano@jax.org>
2019-07-23 12:06:59 -0400

    Popover, cleanup

--- src/views/TextAnnotator.vue
+++ src/views/TextAnnotator.vue
@@ -32,6 +32,11 @@
                                 </div>
                                 <div class="content">
                                         <div class="step1" v-if="!resultsStep">
+
+                                            <b-link @click="populateExample">
+                                                <i class="fa fa-arrow-circle-down" aria-hidden="true"></i> Try example
+                                            </b-link>
+                                            <br>
                                             <b-form-textarea
                                                     id="textarea"
                                                     v-model="form.message"
@@ -41,9 +46,19 @@
                                                     :class="['textarea', ($v.form.message.$error) ? 'is-danger' : '']"
                                             >
                                             </b-form-textarea>
+                                            <br>
+                                            <b-form-checkbox
+                                                    id="checkbox-1"
+                                                    v-model="longestOnly"
+                                                    name="checkbox-1"
+                                                    value="true"
+                                                    unchecked-value="false"
+                                            >
+                                                Always use longest match.
+                                            </b-form-checkbox>
                                         </div>
                                         <div class="step2" v-if="resultsStep">
-                                            <div class="step2" v-html="annotatedText"></div>
+                                            <div class="step2" ref="step2text" v-html="annotatedText"></div>
                                         </div>
                                         <div class="spinner" v-if="showSpinner">
                                             <b-spinner type="grow" label="Spinning"></b-spinner>
@@ -57,7 +72,7 @@
                                     <b-button ref="backButton" v-if="annotatedText" @click="back" :disabled="!validForm" class="stepper-button back">
                                         <i class="fa fa-caret-left fa-fw"></i>Back
                                     </b-button>
-                                    <b-button ref="submitButton" @click="annotateText" :disabled="!validForm" class="stepper-button submit">
+                                    <b-button ref="submitButton" v-if="!resultsStep" @click="annotateText" :disabled="!validForm" class="stepper-button submit">
                                         Submit<i class="fa fa-caret-right fa-fw"></i>
                                     </b-button>
                                 </div>
@@ -92,6 +107,7 @@
                 dataStep: true,
                 errorAnnotating: false,
                 showSpinner: false,
+                longestOnly: false,
 
             };
         },
@@ -111,6 +127,38 @@
                     // We want to allow submission
                     this.$refs.submitButton.attributes
                 }
+            },
+            annotatedText(val){
+                if(val){
+                    const componentref = this;
+                    this.$nextTick(()=>{
+                        Array.from(this.$refs.step2text.children).forEach(child => {
+                        child.addEventListener('mouseenter', function(elem) {
+                            // If a popover container doesnt exist, create append and show,
+                            // otherwise just show.
+                            const annotation = elem.target;
+                            const rect = annotation.getBoundingClientRect();
+                            const left = rect.left + window.scrollX;
+                            if(annotation.children.length === 0){
+                                const popover = componentref.buildPopover("Annotation", annotation.getAttribute('data-sciGraph'));
+                                popover.setAttribute('style', "left:" + left + "px !important;");
+                                annotation.appendChild(popover);
+                            } else {
+                                const popover = annotation.children[0];
+                                popover.classList.remove('hide-popover');
+                                popover.classList.add('show-popover');
+                            }
+                            }, false);
+
+                        child.addEventListener('mouseleave', function(elem) {
+                            const annotation = elem.target;
+                            const popover = annotation.children[0];
+                            popover.classList.remove('show-popover');
+                            popover.classList.add('hide-popover');
+                        }, false);
+                        });
+                    })
+                }
             }
         },
         validations: {
@@ -121,20 +169,6 @@
             }
         },
         computed: {
-            enterAnimation() {
-                if (this.resultsStep === true && this.dataStep === false) {
-                    return "animated quick fadeInLeft";
-                } else {
-                    return "animated quick fadeInRight";
-                }
-            },
-            leaveAnimation() {
-                if (this.resultStep === false && this.dataStep === true) {
-                    return "animated quick fadeOutLeft";
-                } else {
-                    return "animated quick fadeOutRight";
-                }
-            }
         },
         mounted() {
             if(!this.$v.$invalid) {
@@ -144,29 +178,6 @@
             }
         },
         methods: {
-            // Executed when @completed-step event is triggered
-            completeStep(payload) {
-                this.demoSteps.forEach((step) => {
-                    if (step.name === payload.name) {
-                        step.completed = true;
-                    }
-                })
-            },
-            // Executed when @active-step event is triggered
-            isStepActive(payload) {
-                this.demoSteps.forEach((step) => {
-                    if (step.name === payload.name) {
-                        if(step.completed === true) {
-                            step.completed = false;
-                        }
-                    }
-                })
-            },
-            // Executed when @stepper-finished event is triggered
-            alert(payload) {
-                alert('end')
-            },
-
             back() {
                 // Want to go back to the text area.
                 // Reset Annotated Text.
@@ -177,11 +188,19 @@
                 this.errorAnnotating = false;
             },
 
+            populateExample() {
+                this.form.message = 'Lewis (1978) found 7 affected males and 5 affected females in 3 consecutive generations of a Caucasian kindred. As in the X-linked Nettleship-Falls form of ocular albinism (300500), the patients showed reduced visual acuity, photophobia, nystagmus, translucent irides, strabismus, hypermetropic refractive errors, and albinotic fundus with foveal hypoplasia. The skin lesions showed macromelanosomes as in X-linked ocular albinism. Deafness, which was accompanied by vestibular hypofunction, lentigines even in unexposed areas, optic nerve dysplasia, and dominant inheritance distinguished this form of ocular albinism. (In the LEOPARD syndrome (151100) vestibular function is normal.)\n' +
+                    '\n' +
+                    'Bard (1978) described a kindred that was atypical of Waardenburg syndrome (see 193510) in several ways. Although the nasal root was prominent, one affected person had dystopia of the inner canthi or lower puncta. The face in some showed striking freckling of pale skin. Symptomatic vestibular disturbance was another unusual feature. Lewis (1989) expressed the opinion that the family reported by Bard (1978) as an instance of Waardenburg syndrome in fact had this disorder. Lewis (1989) had also been told of 2 other small families with the syndrome. Goldberg (1966) described a Waardenburg syndrome family with apparent ocular albinism.\n' +
+                    '\n' +
+                    'Morell et al. (1997) presented an update of the clinical findings in the family of Bard (1978). The deafness was sensorineural and congenital. Heterochromia iridis was a prominent feature in 1 sibship in which both segmental iris bicolor and complete heterochromia occurred. Most of the affected individuals showed transillumination defects of the iris. Hypopigmentation of the fundus was mild in some, moderate in others, and severe in yet others. Almost all affected individuals had strabismus and visual acuity defects. One individual with a prominent white forelock, characteristic of Waardenburg syndrome, was pictured.';
+            },
+
             async annotateText(){
-                this.resultsStep = true;
                 this.dataStep = false;
+                this.resultsStep = true;
                 this.showSpinner = true;
-                const at = await biolink.annotateText(this.form.message, false);
+                const at = await biolink.annotateText(this.form.message, this.longestOnly);
                 this.showSpinner = false;
                 if (at.status !== 200) {
                     this.errorAnnotating = true;
@@ -189,6 +208,31 @@
                     // Call some endpoint and wait for a response. If, then return then
                     this.annotatedText = at.data;
                 }
+
+            },
+
+            buildPopover(title, data) {
+                // build the raw html popover..
+                let popoverContainer = document.createElement('div');
+                popoverContainer.setAttribute('class', 'popover show-popover');
+                let header = document.createElement('h5');
+                header.innerHTML = title;
+                header.setAttribute('class', 'popover-header');
+                let body = document.createElement('div');
+                body.setAttribute('class', 'popover-body');
+                
+                const annotations = data.split('|');
+                annotations.forEach(annotation => {
+                    let finalBuiltAnnotation = '';
+                    annotation = annotation.split(',');
+                    finalBuiltAnnotation +=  "<div class='annotation'>" + '<span class=\"ontology-id\"> ' + annotation[1] + '</span>';
+                    finalBuiltAnnotation +=  annotation[0] + '</div>';
+                    body.innerHTML += finalBuiltAnnotation;
+                });
+
+                popoverContainer.appendChild(header);
+                popoverContainer.appendChild(body);
+                return popoverContainer;
             }
         }
     };
@@ -203,6 +247,34 @@
             overflow: hidden;
             margin: 5rem 0 3.5rem 0;
             min-height: 200px;
+
+            .sciCrunchAnnotation{
+                text-decoration: underline;;
+                font-weight: bold;
+                color: #39a5dc;
+                cursor: pointer;
+            }
+            .popover {
+                max-width: 450px !important;
+                top: unset !important;
+
+                .annotation {
+                    font-weight: bold;
+                }
+                .ontology-id {
+                    font-size: 12px;
+                    color: gray;
+                    margin-right: 5px;
+                }
+            }
+
+            .show-popover {
+                display: block;
+            }
+
+            .hide-popover {
+                display: none;
+            }
 
             .step1 {
                 text-align: justify;
