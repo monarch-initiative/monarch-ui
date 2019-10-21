bb1abf53fe4d7b291716660c61bf23c4a75625ca
Kent Shefchek <kshefchek@gmail.com>
2019-10-21 09:39:03 -0700

    change development to api prod

--- src/api/BioLink.js
+++ src/api/BioLink.js
@@ -22,7 +22,7 @@ const servers = {
     'search_url': 'https://solr-dev.monarchinitiative.org/solr/search/',
     'owlsim_services_url': 'https://beta.monarchinitiative.org/owlsim',
     'analytics_id': '',
-    'biolink_url': 'https://api-dev.monarchinitiative.org/api/',
+    'biolink_url': 'https://api.monarchinitiative.org/api/',
   },
 
   production: {
