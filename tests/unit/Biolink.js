e3a5238136abe86211917fe77506f4994dfb6d70
Justin Reese <justaddcoffee+github@gmail.com>
2019-12-13 16:09:43 -0800

    Remove lambda from unit test, which is evidently a no-no

--- tests/unit/Biolink.js
+++ tests/unit/Biolink.js
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import * as biolinkService from "@/api/BioLink";
 
 describe("getSources", () => {
-    it("returns array with objects for each source", async() => {
+    it("returns array with objects for each source", async function() {
         const sourcesData = await biolinkService.getSources();
         expect(sourcesData).to.not.be.empty;
         expect(sourcesData).to.be.a("array");
