960fb4518c8aa087a044471d2f5c5eea0c3d2f3c
Justin Reese <justaddcoffee+github@gmail.com>
2019-12-20 12:39:59 -0800

    Lint

--- tests/unit/Biolink.js
+++ tests/unit/Biolink.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import * as biolinkService from '@/api/BioLink';
 
 describe('getSources', () => {
-  it('returns array with objects for each source', async function () {
+  it('returns array with objects for each source', async function fn() {
     const sourcesData = await biolinkService.getSources();
     expect(sourcesData).to.be.a('array');
     expect(sourcesData[0]).to.be.a('object');
