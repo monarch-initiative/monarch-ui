e8feb0125e9eea41b7557f2a1adec10b03c4648b
Justin Reese <justaddcoffee+github@gmail.com>
2019-12-20 12:37:15 -0800

    Get rid of jslint-unfriendly 'expect'

--- tests/unit/Biolink.js
+++ tests/unit/Biolink.js
@@ -4,7 +4,6 @@ import * as biolinkService from '@/api/BioLink';
 describe('getSources', () => {
   it('returns array with objects for each source', async function () {
     const sourcesData = await biolinkService.getSources();
-    expect(sourcesData).to.not.be.empty;
     expect(sourcesData).to.be.a('array');
     expect(sourcesData[0]).to.be.a('object');
     expect(sourcesData[0].sourceDisplayName).to.be.a('string');
