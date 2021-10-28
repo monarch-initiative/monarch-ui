c5427c152f2a2b929791645de479d3cf9202f1c8
Kent Shefchek <kshefchek@gmail.com>
2021-10-27 22:07:45 -0400

    Add a category boost for disease and phenotype (#470)
    * add a category boost for disease and phenotype
    
    * add boost to autocomplete

--- src/api/bio-link.js
+++ src/api/bio-link.js
@@ -389,6 +389,8 @@ export async function getSearchResults(query, start, rows, categories, taxa) {
   params.append("start", start);
   params.append("rows", rows);
   params.append("highlight_class", "hilite");
+  params.append("boost_q", "category:disease^5");
+  params.append("boost_q", "category:phenotype^5");
   params.append("boost_q", "category:genotype^-10");
   params.append("boost_q", "category:variant^-35");
   params.append("boost_q", "category:publication^-10");
@@ -428,6 +430,8 @@ export async function getSearchTermSuggestions(
   params.append("rows", 10);
   params.append("start", 0);
   params.append("highlight_class", "hilite");
+  params.append("boost_q", "category:disease^5");
+  params.append("boost_q", "category:phenotype^5");
   params.append("boost_q", "category:genotype^-10");
   params.append("boost_q", "category:variant^-35");
   params.append("boost_q", "category:publication^-10");
